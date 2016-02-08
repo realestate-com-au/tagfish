3f8736608e154faeeea464d25f62d874af2ee505
clabbe <clement.labbe@rea-group.com>
2016-02-09 10:33:29 +1100

    Move search logic into 2 separate functions

--- lib/tagfish/search_command.rb
+++ lib/tagfish/search_command.rb
@@ -14,19 +14,26 @@ module Tagfish
       docker_api = DockerAPI.new(docker_uri)
       
       if docker_api.api_version == 'v2'
-        repos = docker_api.catalog_v2["repositories"]
-        if keyword
-          repos.select! {|repo| repo.include? keyword}
-        end
-        repos.each {|repo| puts "#{docker_uri.registry}/#{repo}"} 
-      else # 'v1'
-        if not keyword
-          abort("You need to specify a keyword to search a Registry V1")
-        end
-        repos_raw = docker_api.search_v1(keyword)
-        repos = repos_raw["results"].map {|result| result["name"]}
-        puts repos
+        puts search_v2(keyword, docker_api, docker_uri)
+      else
+        puts search_v1(keyword, docker_api)
       end
+    end
+    
+    def search_v2(keyword, docker_api, docker_uri)
+      repos = docker_api.catalog_v2["repositories"]
+      if keyword
+        repos.select! {|repo| repo.include? keyword}
+      end
+      repos.map {|repo| "#{docker_uri.registry}/#{repo}"} 
+    end
+    
+    def search_v1(keyword, docker_api)
+      if not keyword
+        abort("You need to specify a keyword to search a Registry V1")
+      end
+      repos_raw = docker_api.search_v1(keyword)
+      repos = repos_raw["results"].map {|result| result["name"]}
     end
   end
 end
