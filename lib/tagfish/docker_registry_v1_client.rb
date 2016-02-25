782a8c625fccfcda1be323ff731f6a1e811f781b
clabbe <clement.labbe@rea-group.com>
2016-02-25 15:31:31 +1100

    Split v1 and v2 logic

--- lib/tagfish/docker_registry_v1_client.rb
+++ lib/tagfish/docker_registry_v1_client.rb
@@ -7,5 +7,49 @@ module Tagfish
       'v1'
     end
     
+    def find_tags_by_repository(tags_only=false)
+      tags_list = tags_v1_logic
+      Tagfish::Tags.new(tags_list)
+    end
+    
+    def tags_v1
+      APICall.new(tags_v1_uri).get_json(http_auth)
+    end
+    
+    def search_v1(keyword)
+      APICall.new(search_v1_uri(keyword)).get_json(http_auth)
+    end
+    
+    private
+    
+    def tags_v1_logic
+      tags_json = tags_v1
+      tags_v1_api(tags_json)
+    end
+    
+    def tags_v1_api(api_response_data)
+      case api_response_data
+        when Hash
+        api_response_data
+      when Array
+        api_response_data.reduce({}) do |images, tag|
+          images.merge({tag["name"] => tag["layer"]})
+        end
+      else
+        raise "unexpected type #{api_response_data.class}"
+      end
+    end
+    
+    def ping_uri
+      "#{base_uri}/v1/_ping"
+    end
+    
+    def search_v1_uri(keyword)
+      "#{base_uri}/v1/search?q=#{keyword}"  
+    end
+    
+    def tags_v1_uri
+      "#{base_uri}/v1/repositories/#{docker_uri.repository}/tags"
+    end
   end
 end
--- lib/tagfish/docker_registry_v2_client.rb
+++ lib/tagfish/docker_registry_v2_client.rb
@@ -6,6 +6,56 @@ module Tagfish
     def api_version
       'v2'
     end
+    
+    def find_tags_by_repository(tags_only=false)
+      tags_list = tags_v2_logic(tags_only)
+      Tagfish::Tags.new(tags_list)
+    end
+    
+    def tags_v2
+      APICall.new(tags_v2_uri).get_json(http_auth)
+    end
+    
+    def hash_v2(tag)
+      APICall.new(hash_v2_uri(tag)).get_json(http_auth)
+    end
+    
+    def catalog_v2
+      APICall.new(catalog_v2_uri).get_json(http_auth)
+    end
 
+    private
+    
+    def tags_v2_logic(tags_only)
+      tags = tags_v2["tags"]
+      if tags.nil?
+        abort("No Tags found for this repository")
+      end
+      
+      tags_with_hashes = tags.inject({}) do |dict, tag|
+        if tags_only
+          dict[tag] = "dummy_hash"
+        else
+          dict[tag] = hash_v2(tag)["fsLayers"][0]["blobSum"]
+        end
+        dict
+      end
+    end
+      
+    def ping_uri
+      "#{base_uri}/v2/"
+    end
+
+    def catalog_v2_uri
+      "#{base_uri}/v2/_catalog"
+    end
+    
+    def tags_v2_uri
+      "#{base_uri}/v2/#{docker_uri.repository}/tags/list"  
+    end
+    
+    def hash_v2_uri(tag)
+      "#{base_uri}/v2/#{docker_uri.repository}/manifests/#{tag}"
+    end
   end
 end
